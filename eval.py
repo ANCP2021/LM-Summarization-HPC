053658be353ec42e23ed9a43f097931a5556fb15
Alexander Nemecek <ajn98@case.edu>
2024-07-30 22:43:00 -0600

    rouge and bert scores

--- eval.py
+++ eval.py
@@ -1,14 +1,27 @@
 import evaluate
+import bert_score
 import json
+
+def rouge_score(generated, reference):
+    rouge_score = evaluate.load("rouge")
+    scores = rouge_score.compute(predictions=generated, references=reference)
+    return scores
+
+def bert_score(generated, reference):
+    precision, recall, f1 = bert_score.score(generated_summaries, reference_summaries, lang="en")
+    return precision, recall, f1
 
 if __name__ == '__main__':
     file_name = "google_flan-t5-xl_abisee_cnn_dailymail_summaries.json"
     with open(file_name, 'r') as json_file:
         data = json.load(json_file)
 
-    rouge_score = evaluate.load("rouge")
     generated_summaries = [sample['generated'] for sample in data]
     reference_summaries = [sample['reference'] for sample in data]
     
-    scores = rouge_score.compute(predictions=generated_summaries, references=reference_summaries)
-    print(scores)
+    rouge_scores = rouge_score(generated_summaries, reference_summaries)
+
+    precision_bert, recall_bert, f1_bert = bert_score(generated_summaries, reference_summaries)
+    # print(f"Precision: {precision_bert.mean().item()}")
+    # print(f"Recall: {recall_bert.mean().item()}")
+    # print(f"F1: {f1_bert.mean().item()}")
