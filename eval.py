95d258ee5b5791106f5c94871545333670805cdd
Alexander Nemecek <ajn98@case.edu>
2024-07-30 22:17:54 -0600

    baseline eval

--- eval.py
+++ eval.py
@@ -2,13 +2,13 @@ import evaluate
 import json
 
 if __name__ == '__main__':
-    file_name = "google_flan-t5-xlabisee_cnn_dailymailsummaries.json"
+    file_name = "google_flan-t5-xl_abisee_cnn_dailymail_summaries.json"
     with open(file_name, 'r') as json_file:
         data = json.load(json_file)
 
-    print(data)
-    quit()
     rouge_score = evaluate.load("rouge")
-    scores = rouge_score.compute(
-        predictions=[generated_summary], references=[reference_summary]
-    )
+    generated_summaries = [sample['generated'] for sample in data]
+    reference_summaries = [sample['reference'] for sample in data]
+    
+    scores = rouge_score.compute(predictions=generated_summaries, references=reference_summaries)
+    print(scores)
