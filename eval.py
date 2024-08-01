bc6417a58872d02bbfbdc58ec21cdacd94e4e000
Alexander Nemecek <ajn98@case.edu>
2024-08-01 12:43:56 -0600

    small change to eval

--- eval.py
+++ eval.py
@@ -7,12 +7,12 @@ def rouge_score(generated, reference):
     scores = rouge_score.compute(predictions=generated, references=reference)
     return scores
 
-def bert_score(generated, reference):
-    precision, recall, f1 = bert_score.score(generated_summaries, reference_summaries, lang="en")
+def bert(generated, reference):
+    precision, recall, f1 = bert_score.score(generated, reference, lang="en")
     return precision, recall, f1
 
 if __name__ == '__main__':
-    file_name = "google_flan-t5-xl_abisee_cnn_dailymail_summaries.json"
+    file_name = "./inference_outputs/google_flan-t5-xl_abisee_cnn_dailymail_summaries.json"
     with open(file_name, 'r') as json_file:
         data = json.load(json_file)
 
@@ -20,8 +20,8 @@ if __name__ == '__main__':
     reference_summaries = [sample['reference'] for sample in data]
     
     rouge_scores = rouge_score(generated_summaries, reference_summaries)
-
-    precision_bert, recall_bert, f1_bert = bert_score(generated_summaries, reference_summaries)
-    # print(f"Precision: {precision_bert.mean().item()}")
-    # print(f"Recall: {recall_bert.mean().item()}")
-    # print(f"F1: {f1_bert.mean().item()}")
+    print(rouge_scores)
+    precision_bert, recall_bert, f1_bert = bert(generated_summaries, reference_summaries)
+    print(f"Precision: {precision_bert.mean().item()}")
+    print(f"Recall: {recall_bert.mean().item()}")
+    print(f"F1: {f1_bert.mean().item()}")
